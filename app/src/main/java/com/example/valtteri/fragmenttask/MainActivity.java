5a7403fce3962ac99093eed8c999c452f2b5f35e
valttepe <valtteri.pesu@hotmail.com>
2017-08-27 19:22:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/valtteri/fragmenttask/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.valtteri.fragmenttask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            WriteFragment writeFrag = new WriteFragment();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, writeFrag).commit();
+        }
+
+        final Button writebtn = (Button)findViewById(R.id.writeFragment);
+        writebtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ChangeFragment(v);
+            }
+        });
+
+        final Button readbtn = (Button)findViewById(R.id.readFragment);
+        readbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ChangeFragment(v);
+            }
+        });
+
+    }
+
+    public void ChangeFragment(View view) {
+        Fragment fragment;
+
+        if ( view == findViewById(R.id.readFragment)){
+            fragment = new ReadFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_container, fragment);
+
+            ft.commit();
+        }
+        else if (view == findViewById(R.id.writeFragment)) {
+            fragment = new WriteFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_container, fragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/valtteri/fragmenttask/ReadFragment.java
@@ -0,0 +1,22 @@
+package com.example.valtteri.fragmenttask;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Valtteri on 27/08/2017.
+ */
+
+public class ReadFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_read, container, false);
+    }
+}
